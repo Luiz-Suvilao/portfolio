d3460e118c7a9d6ce5bec1ccca760a05280f4040
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2023-04-09 04:31:48 -0300

    adjust: header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,43 +12,45 @@ const Header = () => {
 
     return (
         <header className={`${styles.container} ${isDarkTheme ? styles.dark : styles.light}`}>
-            <h1 className={styles.title}>LF</h1>
+            <div className={styles.content}>
+                <h1 className={styles.title}>LF</h1>
 
-            <nav>
-                <ul className={styles.linkList}>
-                    <li>
-                        <Link href="#">
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="#">
-                            Sobre
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="#">
-                            Tecnologias
-                        </Link>
-                    </li>
+                <nav>
+                    <ul className={styles.linkList}>
+                        <li>
+                            <Link href="#">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#">
+                                Sobre
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#">
+                                Tecnologias
+                            </Link>
+                        </li>
 
-                    <li>
-                        <Link target="_blank" href="https://github.com/Luiz-Suvilao">
-                            <FaGithub />
-                        </Link>
-                    </li>
+                        <li>
+                            <Link target="_blank" href="https://github.com/Luiz-Suvilao">
+                                <FaGithub />
+                            </Link>
+                        </li>
 
-                    <li>
-                        <Link target="_blank" href="https://www.linkedin.com/in/luiz-filipe-da-silva-de-jesus-490a02182/">
-                            <FaLinkedin />
-                        </Link>
-                    </li>
+                        <li>
+                            <Link target="_blank" href="https://www.linkedin.com/in/luiz-filipe-da-silva-de-jesus-490a02182/">
+                                <FaLinkedin />
+                            </Link>
+                        </li>
 
-                    <li>
-                        <SwitchTheme />
-                    </li>
-                </ul>
-            </nav>
+                        <li>
+                            <SwitchTheme />
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
