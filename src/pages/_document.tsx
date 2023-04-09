53b4bed397fe37fc8b5406ee7eb20a0fb817e8ba
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2023-04-09 16:58:37 -0300

    feat: description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang='pt-BR'>
-            <Head />
+            <Head>
+                <meta name="description" content="Meu site de apresentação pessoal." />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
